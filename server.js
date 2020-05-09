59c7ef95d678580c5baf06d5f3ab29bb8cea8e76
Bob Bass <bob@bbass.co>
2020-05-09 16:34:50 -0400

    setup server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const dotEnv = require("dotenv");
+const cors = require("cors");
 
 dotEnv.config();
 
 const app = express();
+
+// CORS allows for cross origin API calls
+app.use(cors());
+
+// Create Middleware
+const myMiddleware = (req, res, next) => {
+  console.log("Sample");
+  next();
+};
+
+// Request Payload Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   res.send("Hello World");
@@ -13,4 +27,15 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
+});
+
+// Global error handling middleware
+// Errors will fall-back here if they're missed elsewhere
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send({
+    status: 500,
+    message: err.message,
+    body: {}
+  });
 });
