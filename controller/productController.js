bd82cc185b84010993203380e9c9eede6c9d79b8
Bob Bass <bob@bbass.co>
2020-05-09 18:33:38 -0400

    added product contants

--- controller/productController.js
+++ controller/productController.js
@@ -1,18 +1,17 @@
 const productService = require("../service/productService");
+const constants = require("../constants");
 
 module.exports.createProduct = async (req, res) => {
-  let response = {};
+  let response = { ...constants.defaultServerResponse };
   try {
     // Returns data to client
     const responseFromService = await productService.createProduct(req.body);
     response.status = 200;
-    response.message = "Product Created Successfully";
+    response.message = constants.productMessage.PRODUCT_CREATED;
     response.body = responseFromService;
   } catch (error) {
     console.log("Something went wrong: Controller: productController", error);
-    response.status = 400;
     response.message = error.message;
-    response.body = responseFromService;
   }
   // Send full response object
   return res.status(response.status).send(response);
--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add product to MongoDB with POST
+// Request Hits Server → Server Forwards API requests to /routes/appendedRoute → Type of HTTP Method is defined in Route → Request Handed off to Controller → Controller Sends Data to Service (productService) and Response to the Client (Insomnia at the moment) → productService inserts data into database (Using Model as a reference) → Result of  Database Read/Write sent back to Controller to respond to client accordingly
 app.use("/api/v1/product", require("./routes/productRoutes"));
 
 // Routing
