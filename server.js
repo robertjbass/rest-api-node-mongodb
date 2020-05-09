96247157a65f1f731a17b458dce7846a09342b41
Bob Bass <bob@bbass.co>
2020-05-09 16:22:06 -0400

    myCommit 4

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get("/", (req, res, next) => {
   res.send("Hello World");
 });
 
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
 });
