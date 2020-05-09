ddd0f5a92b10a227c4641a7b3812a60d04628684
Bob Bass <bob@bbass.co>
2020-05-09 17:39:56 -0400

    model and controller created

---
+++ controller/productController.js
@@ -0,0 +1,3 @@
+module.exports.createProduct = (req, res) => {
+  res.send("Product Created Successfully");
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../controller/productController");
 
 // TODO: POST API
 
-router.post("/");
+router.post("/", productController.createProduct);
 
 module.exports = router;
